53bdd1fede19f3ae4f6253d190afafefa06a8f22
Alex Wohlbruck <alexwohlbruck@gmail.com>
2022-03-19 01:41:05 -0400

    Add blog posts and members to schema

--- src/api/worxstr-blog/controllers/worxstr-blog.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  worxstr-blog controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::worxstr-blog.worxstr-blog');
--- src/api/worxstr-blog/services/worxstr-blog.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * worxstr-blog service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::worxstr-blog.worxstr-blog');

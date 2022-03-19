3eafd16138ff897df30bf52c3b405e611553c2a3
Jackson Sippe <jackson123198@gmail.com>
2022-03-18 20:11:36 -0600

    DB conn and S3 working. Basic blog structure in place

---
+++ src/api/worxstr-blog/routes/worxstr-blog.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * worxstr-blog router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::worxstr-blog.worxstr-blog');
---
+++ src/api/worxstr-blog/services/worxstr-blog.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * worxstr-blog service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::worxstr-blog.worxstr-blog');

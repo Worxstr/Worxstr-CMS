8b6e8d5a20452b6196bbc9135e5b90b15e2b4501
Alex Wohlbruck <alexwohlbruck@gmail.com>
2022-05-29 15:45:15 -0400

    Add Support Article to cms

---
+++ src/api/support-article/routes/support-article.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * support-article router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::support-article.support-article');
